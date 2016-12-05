678df644ccce46a1e6a8c5ad67bd740485521cf2
Stas Vilchik <vilchiks@gmail.com>
2016-12-05 16:42:13 +0100

    SONAR-8451 add db migration check

---
+++ server/sonar-web/src/main/js/app/components/MigrationContainer.js
@@ -0,0 +1,51 @@
+/*
+ * SonarQube
+ * Copyright (C) 2009-2016 SonarSource SA
+ * mailto:contact AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+// @flow
+import React from 'react';
+import { getSystemStatus } from '../../api/system';
+import handleRequiredMigration from '../utils/handleRequiredMigration';
+
+export default class MigrationContainer extends React.Component {
+  static propTypes = {
+    children: React.PropTypes.element.isRequired
+  };
+
+  state = {
+    loading: true
+  };
+
+  componentDidMount () {
+    getSystemStatus().then(r => {
+      if (r.status === 'UP') {
+        this.setState({ loading: false });
+      } else {
+        handleRequiredMigration();
+      }
+    });
+  }
+
+  render () {
+    if (this.state.loading) {
+      return null;
+    }
+
+    return this.props.children;
+  }
+}
